65de7b61fa957d7e71b7117750955ccce818caac
Ivan Kuznetsov <me@jeiwan.ru>
2019-03-20 11:32:41 +0700

    Rename environment.go to makeos.go

--- makeos.go
+++ makeos.go
@@ -1,3 +1,45 @@
 package makeos
 
-const localKeosdURL = "http://127.0.0.1:8899"
+import (
+	eosgo "github.com/eoscanada/eos-go"
+)
+
+// Environment ...
+type Environment struct {
+	Autostart      bool
+	NodeosURL      string
+	KeosURL        string
+	Wallet         string
+	WalletPassword string
+}
+
+// DevEnvironment ...
+var DevEnvironment = &Environment{
+	Autostart:      true,
+	NodeosURL:      "http://127.0.0.1:8888",
+	KeosURL:        "http://127.0.0.1:8899",
+	Wallet:         "dev",
+	WalletPassword: "password",
+}
+
+// WithEnvironment ...
+func WithEnvironment(environment *Environment, body func()) {
+	nodeos = newNodeos(environment.NodeosURL)
+	keos = newKeos(
+		environment.KeosURL,
+		environment.Wallet,
+		environment.WalletPassword,
+	)
+	nodeos.Client.SetSigner(eosgo.NewWalletSigner(
+		keos.Client,
+		keos.Wallet,
+	))
+
+	defer nodeos.Cleanup()
+
+	if environment.Autostart {
+		nodeos.Start()
+	}
+
+	body()
+}
